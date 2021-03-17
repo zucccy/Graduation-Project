81c1021003560e6ea21f3c4677ff91dd2c747feb
吴智宏 <wuzhihong0619@gmail.com>
2021-03-17 22:41:34 +0800

    first commit web

---
+++ gp-web/Medical/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  #app {
+    padding-top: 55px;
+    background: #f0f2f5;
+  }
+
+  body {
+    margin: 0;
+    background: #f0f2f5;
+  }
+</style>
